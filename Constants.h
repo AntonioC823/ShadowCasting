a519fa857a35d399b8f5f0242445aa0c46ce8b1e
AntonioC823 <61690421+AntonioC823@users.noreply.github.com>
2020-03-06 05:53:03 -0800

    ShadowCasting

---
+++ Constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+const int FRAME_RATE = 60;
+const int BLOCK_WIDTH = 40;
+const int WORLD_WIDTH = WINDOW_WIDTH / BLOCK_WIDTH;
+const int WORLD_HEIGHT = WINDOW_HEIGHT / BLOCK_WIDTH;
